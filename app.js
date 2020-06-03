2a7d446f25ca2b68402bef4a39a0df5ff85cb714
Kyoungsik Choi <kyoungsik.dev@gmail.com>
2020-06-04 00:21:52 +0900

    Axios, Cheerio 추가 및 크롤링 준비

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const cheerio = require('cheerio');
 
+const log = console.log;
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+
+  // -------- 날짜 표시 --------
+  const date = {};
   const numbers = [0, 1, 2, 3, 4]; // Monday to Friday
-  const weekdayString = ['월', '화', '수', '목', '금'];
   const today = new Date();
-  const weekdayToday = 2;
+  date.weekdayString = ['월', '화', '수', '목', '금'];
+  date.weekdayToday = 2;
 
   const formatDate = (mydate) => {
     return mydate.getMonth()+1 + '월 ' + mydate.getDate() + '일';
   }
 
-  const datearr = numbers.map(index => {
-    const day = new Date(today.getTime() - ((today.getDay() - index - 1) * 86400 * 1000));
-    return formatDate(day);
+  const dateObjs = numbers.map(index => {
+    return new Date(today.getTime() - ((today.getDay() - index - 1) * 86400 * 1000));
+  });
+  date.dateString = dateObjs.map(o => formatDate(o));
+
+  // -------- 식단 크롤링 --------
+
+  const urls = dateObjs.map(o => {
+    const thisDate = `${o.getFullYear()}-${o.getMonth()+1}-${o.getDate}`;
+    return 'https://www.skhystec.com/service-cafe-bd.asp?m_gubun1=2&shr_yymmdd=' + thisDate;
   })
+  
+  // urls로 GET 요청 및 HTML 파싱
 
 
   res.render('index', {
-    datearr, 
-    weekdayString,
-    weekdayToday
+    date
   });
 });
 
